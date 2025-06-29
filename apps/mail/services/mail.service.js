011ec36cbf742c9bb0e74ec772ae3e7e698a6375
EdenIvgi <edenavgi@gmail.com>
2025-06-29 19:52:46 +0300

    Added mail service

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -1,1 +1,102 @@
-// mail service
+import { loadFromStorage, makeId, saveToStorage } from './util.service.js'
+import { storageService } from './async-storage.service.js'
+
+const MAIL_KEY = 'mailDB'
+_createMails()
+
+export const mailService = {
+    query,
+    get,
+    remove,
+    save,
+    getEmptyMail,
+    getDefaultFilter,
+    getFilterFromSearchParams
+}
+
+function query(filterBy = {}) {
+    return storageService.query(MAIL_KEY)
+        .then(mails => {
+            if (filterBy.txt) {
+                const regExp = new RegExp(filterBy.txt, 'i')
+                mails = mails.filter(mail => regExp.test(mail.vendor))
+            }
+            if (filterBy.minSpeed) {
+                mails = mails.filter(mail => mail.speed >= filterBy.minSpeed)
+            }
+            // console.log(' mails:', mails)
+            return mails
+        })
+}
+
+function get(mailId) {
+    return storageService.get(MAIL_KEY, mailId).then(_setNextPrevMailId)
+}
+
+function remove(mailId) {
+    // return Promise.reject('Oh No!')
+    return storageService.remove(MAIL_KEY, mailId)
+}
+
+function save(mail) {
+    if (mail.id) {
+        return storageService.put(MAIL_KEY, mail)
+    } else {
+        return storageService.post(MAIL_KEY, mail)
+    }
+}
+
+function getEmptyMail(subject = '') {
+    return { subject }
+}
+
+function getDefaultFilter() {
+    return { txt: '' }
+}
+
+
+
+function _createMails() {
+    let mails = loadFromStorage(MAIL_KEY)
+    if (!mails || !mails.length) {
+        mails = [
+            _createMail('audu', 300),
+            _createMail('fiak', 120),
+            _createMail('subali', 50),
+            _createMail('mitsu', 150)
+        ]
+        saveToStorage(MAIL_KEY, mails)
+    }
+}
+
+function _createMail(vendor) {
+    const mail = getEmptyMail(vendor)
+    mail.id = makeId()
+    return mail
+}
+
+
+
+function getFilterFromSearchParams(searchParams) {
+    const txt = searchParams.get('txt') || ''
+
+    return {
+        txt,
+    }
+}
+
+
+
+function _setNextPrevMailId(mail) {
+    return query().then((mails) => {
+        const mailIdx = mails.findIndex((currMail) => currMail.id === mail.id)
+        const nextMail = mails[mailIdx + 1] ? mails[mailIdx + 1] : mails[0]
+        const prevMail = mails[mailIdx - 1] ? mails[mailIdx - 1] : mails[mails.length - 1]
+        mail.nextMailId = nextMail.id
+        mail.prevMailId = prevMail.id
+        return mail
+    })
+}
+
+
+
