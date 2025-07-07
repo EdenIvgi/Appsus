7432938a50e16534cc581a9bf4b7551fb82cc7f1
amiravni16 <amiravni13@gmail.com>
2025-07-07 20:45:50 +0300

    Fix note titles design

--- apps/note/cmps/NoteTxt.jsx
+++ apps/note/cmps/NoteTxt.jsx
@@ -1,6 +1,7 @@
 export function NoteTxt({ info }) {
     return (
         <div className="note-txt">
+            {info.title && <h3>{info.title}</h3>}
             <p>{info.txt}</p>
         </div>
     )
