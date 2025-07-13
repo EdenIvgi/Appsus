1360b93aea840a17e79775bc13c35d13e4bf40d5
amiravni16 <amiravni13@gmail.com>
2025-07-13 17:03:43 +0300

    Add navigation to main homepage from book app

--- apps/book/pages/Lesson34 - missbooks/cmps/AppHeader.jsx
+++ apps/book/pages/Lesson34 - missbooks/cmps/AppHeader.jsx
@@ -1,9 +1,14 @@
-const { NavLink } = ReactRouterDOM;
+const { NavLink, Link } = ReactRouterDOM;
 
 export function AppHeader() {
 	return (
 		<header className="app-header">
-			<h1>Miss Books</h1>
+			<div className="header-left">
+				<Link to="/" className="appsus-logo-btn">
+					<span className="material-symbols-outlined">orbit</span>
+				</Link>
+				<h1>Miss Books</h1>
+			</div>
 			<nav>
 				<NavLink to="/book">Home</NavLink>
 				<NavLink to="/book/about">About</NavLink>
